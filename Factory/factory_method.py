ac8e57368e4b9b7e358a6d60795d3ad7749946f8
vishvassingh <vishvassingh.10816@gmail.com>
2024-03-10 18:14:00 +0530

    Design principles in python 1. Added prototype 2. Added factories

--- Factory/factory_method.py
+++ Factory/factory_method.py
@@ -5,6 +5,7 @@
 """
 
 from enum import Enum
+from math import *
 
 
 class CoordinateSystem(Enum):
@@ -12,3 +13,41 @@ class CoordinateSystem(Enum):
     POLAR = 2
 
 
+class Point:
+    # def __init__(self, a, b, system=CoordinateSystem.CARTESIAN):
+    #     """
+    #         When the initialization is complex, we do not want to add conditions in init method and this would also
+    #         break the open close principle in case we want to add more co-ordinate systems in future so to simply this
+    #         it is better to use factories which help us with initialization of objects in different ways
+    #     :param a: value of a
+    #     :param b: value of b
+    #     :param system: co-ordinate system
+    #     """
+    # if system == CoordinateSystem.CARTESIAN:
+    #     self.x = a
+    #     self.y = b
+    #
+    # elif system == CoordinateSystem.POLAR:
+    #     self.x = a * sin(b)
+    #     self.y = a * cos(b)
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    @staticmethod
+    def new_cartesian_point(x, y):
+        return Point(x, y)
+
+    @staticmethod
+    def new_polar_point(rho, theta):
+        return Point(rho * cos(theta), rho * sin(theta))
+
+    def __str__(self):
+        return f'x: {self.x}, y: {self.y}'
+
+
+if __name__ == '__main__':
+    p = Point(2, 3)
+    p2 = Point.new_polar_point(2, 3)
+    print(p, p2)
---
+++ Prototype/prototype.py
@@ -0,0 +1,33 @@
+"""
+    Prototype is a partially or fully initialized object that you copy (clone) and make use of
+"""
+
+import copy
+
+
+class Address:
+    def __init__(self, street_address, city, country):
+        self.country = country
+        self.city = city
+        self.street_address = street_address
+
+    def __str__(self):
+        return f'{self.street_address}, {self.city}, {self.country}'
+
+
+class Person:
+    def __init__(self, name, address):
+        self.name = name
+        self.address = address
+
+    def __str__(self):
+        return f'{self.name} lives at {self.address}'
+
+
+john = Person("John", Address("123 London Road", "London", "UK"))
+print(john)
+# jane = john
+jane = copy.deepcopy(john)
+jane.name = "Jane"
+jane.address.street_address = "124 London Road"
+print(john, jane)
